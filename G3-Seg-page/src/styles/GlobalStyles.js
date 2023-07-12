aae674b3e7d374bad0aa2bd750b2855aee9d82e0
Erika <erika.d.azevedo@hotmail.com>
2023-07-12 10:19:30 -0300

    configuração estilo global

--- G3-Seg-page/src/styles/GlobalStyles.js
+++ G3-Seg-page/src/styles/GlobalStyles.js
@@ -2,15 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-  @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap');
-
+@import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
   body {
     background: #242424;
-    width: 100%;
+    /* width: 100%; */
+  }
+
+  *{
+    font-family: 'Lexend', sans-serif;
     margin: 0;
     padding: 0;
-    font-family: 'Lexend', sans-serif;
     box-sizing: border-box;
   }
 
