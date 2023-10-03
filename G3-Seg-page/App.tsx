1dec8df67ca6635846217e45378161fb85dc0715
Erika Diniz <erika.d.azevedo@hotmail.com>
2023-10-03 16:00:24 -0300

    add icones em card de prevencao

--- G3-Seg-page/App.tsx
+++ G3-Seg-page/App.tsx
@@ -3,7 +3,7 @@ import HeaderArea from "./src/components/header/index.js";
 import Servicos from "./src/components/servicos/index.js";
 import HistoriaG3 from "./src/components/historia/index.js";
 import Segmentacao from "./src/components/segmentacao/index.js";
-// import Prevencao from "./src/components/prevencao/index.js";
+import Prevencao from "./src/components/prevencao/index.js";
 // import GanhosProjetadosCard from "./src/components/card-ganhos-projetados/index.js";
 // import ContatoCard from "./src/components/contato/index.js";
 // import FooterArea from "./src/components/footer/index.js";
@@ -16,8 +16,8 @@ function App() {
       <Servicos />
       <HistoriaG3 />
       <Segmentacao />
-      {/* 
       <Prevencao />
+      {/* 
       <GanhosProjetadosCard />
       <ContatoCard />
       <FooterArea /> */}
--- G3-Seg-page/src/components/prevencao/styled.ts
+++ G3-Seg-page/src/components/prevencao/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import img_card_prevencao from "../../assets/img/img-card-prev.svg";
 export const PrevencaoStyle = styled.div`
   .prevencao-card-geral {
-    width: 100%;
+    width: auto;
     height: auto;
     background-image: url(${img_card_prevencao});
     background-repeat: no-repeat;
@@ -16,6 +16,8 @@ export const PrevencaoStyle = styled.div`
   }
 
   .prevencao-card-text {
+    width: 100%;
+    height: auto;
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
@@ -42,33 +44,65 @@ export const PrevencaoStyle = styled.div`
     text-align: center;
   }
 
-  /* .prevencao-card-wrapper {
+  .prevencao-card-wrapper {
     width: 100%;
+    height: auto;
     display: flex;
     justify-content: center;
-  } */
+  }
 
-  //media query
-
-  @media only screen and (max-width: 1023px) {
+  
+  /* Mobile */
+  @media (min-width: 320px) and (max-width: 767px) {
     .prevencao-card-geral {
-      width: 100%;
-      padding-left: 6rem;
-      padding-top: 3rem;
-      padding-bottom: 3rem;
-      background-size: cover;
-      display: flex;
-    }
+    width: auto;
+    height: auto;
+    background-size: cover;
+    padding: 3rem 1rem;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+  }
 
-    .prevencao-card-text {
-      display: flex;
-      align-content: flex-start;
-      align-items: flex-start;
-    }
+  .prevencao-card-text {
+    width:100%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+  }
 
-    .prevencao-card-wrapper {
-      width: 100%;
-      display: flex;
-    }
+  .prevencao-card-h3 {
+    color: #f9b32a;
+    text-align: center;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+
+  .prevencao-card-p {
+    color: #f9b32a;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    width: 45rem;
+    margin-top: 1rem;
+    text-align: center;
+  }
+
+
+  }
+
+  /* Tablet */
+  @media (min-width: 768px) and (max-width: 1024px) {
+
+
+  }
+
+  /* Desktop */
+  @media (min-width: 1025px) {
+
   }
 `;
